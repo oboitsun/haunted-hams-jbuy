0903288f05ef6fac4fdd89a0d2af71c5d76d74fa
Oleksandr Boitsun <thewip3out@gmail.com>
2021-11-16 06:17:05 +0200

    changed portal link

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ export default function Header({
     { href: "about-us", text: "About Us" },
     { href: "team", text: "Team" },
     { href: "roadmap", text: "Roadmap" },
-    { href: "https://hams-portal.vercel.app", text: "Portal" },
+    { href: "https://portal.hauntedhamsters.io", text: "Portal" },
     { href: "faq", text: "FAQ" },
   ];
 
